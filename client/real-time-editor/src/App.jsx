7b0560c3bd3a1dde56e2058dce1ea0d917ff2dec
RISHITH <rishith@gmail.com>
2024-01-10 00:29:35 +0530

    added real time code editing

--- client/real-time-editor/src/App.jsx
+++ client/real-time-editor/src/App.jsx
@@ -1,30 +1,30 @@
-import { io } from "socket.io-client";
-import { useEffect } from "react";
+import socket from "./socket";
+import { useEffect, useState } from "react";
 function App() {
+  const [code, setCode] = useState();
   useEffect(() => {
-    const socket = io('http://localhost:3000');
-    console.log('Message from server:');
-    // Listen for a custom event from the server
-    socket.on('message', (data) => {
-      
+    socket.on("codeChange", (data) => {
+      setCode(data); // set the code to the value received from the server
     });
 
-    // Emit a custom event to the server
-    socket.emit('client-message', 'Hello from the client!');
-    
-
     return () => {
-      console.log("dis")
-      socket.disconnect();
+      // socket.disconnect();
     };
   }, []);
 
-  
+  const handleChange = (e) => {
+    socket.emit("codeChange", e.target.value); // emit the code to the server
+    setCode(e.target.value);
+  };
 
-  return <>
-    <textarea id="document"></textarea>
-    
-  </>;
+  return (
+    <textarea
+      value={code}
+      onChange={handleChange}
+      cols="30"
+      rows="10"
+    ></textarea>
+  );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,38 @@
-const socket = require('socket.io')(3000 , {
-    cors:{
-        origin:['http://localhost:5173']
+const express = require('express');
+const http = require('http');
+const {Server} = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173"
     }
-})
+});
 
-socket.on('connection', soc => {
-    console.log(soc.id)
-})
+// Use the cors middleware to allow all origins
 
-socket.emit('message', 'Serevredtysajcbuasci');
+// Socket.IO connection handling
+io.on('connection', (socket) => {
+  console.log(socket.id);
+  console.log('A user connected');
+
+  // Send a message to the client when a new user connects
+  console.log(socket.emit('messageFromServer', 'Welcome to the server!'));
+
+  socket.on('codeChange', (code) => {
+    console.log('code change detected');
+    socket.broadcast.emit('codeChange',code);
+  });
+
+});
 
 
 
-socket.on('client-message', data => {
-    console.log(data)
-})
+// Your other routes or middleware go here
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
