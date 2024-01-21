a29063e4dbebc8e7ae3f9d1a679252b58c736353
jahnav-sannapureddy <jahnavsannapureddy@gmail.com>
2024-01-22 01:00:37 +0530

    signup-ui

--- client/real-time-editor/src/pages/login/login.jsx
+++ client/real-time-editor/src/pages/login/login.jsx
@@ -4,18 +4,17 @@ const LoginPage = () => {
   return (
     <>
       <div className="flex w-screen h-[calc(100vh-4rem)] justify-center items-center">
-        <div className="flex flex-col m-8 rounded-lg overflow-hidden border-indigo-400 b border-2 justify-center">
-          <span className="text-center pt-2 pb-2 bg-indigo-600 text-white">LOGIN</span>
+        <div className="flex flex-col m-8 rounded-lg overflow-hidden w-96 border-indigo-800 b border-2 justify-center">
+          <span className="text-center py-3 text-lg bg-indigo-800 text-white">LOGIN</span>
           <form className="flex flex-col px-8 my-4">
             <label for="email" className="block mb-2">Email</label>
             <input className="mb-5 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" id="email" type="email" placeholder="example@mail.com"></input>
             <label for="password" className="block mb-2">Password</label>
-            <input className="mb-8 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" type="password" id="password" placeholder="password"></input>
-            <button className="bg-indigo-800 capitalize text-white p-2 w-3/4 self-center rounded-md">Login</button>
+            <input className="mb-8 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" type="password" id="password" placeholder="password"></input>
+            <button className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700">Login</button>
           </form>
-          <Link className="mx-10 my-2" to="/register">Don't have an account? create one</Link>          
+          <Link className="m-2 self-center" to="/register">Don't have an account? <span className=" text-indigo-500">Create One</span></Link>          
         </div>
-        
       </div>
     </>
   );
--- client/real-time-editor/src/pages/register.jsx
+++ client/real-time-editor/src/pages/register.jsx
@@ -1,48 +1,66 @@
 import { useState } from "react";
 import { registerUser } from "../functions/user_functions";
+import { Link } from "react-router-dom"
 
 const RegisterPage = () => {
+  const [message, setMessage] = useState("info");
+  const [showMessage, setShowMessage] = useState(false);
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <div className=" flex flex-col flex-wrap">
-      <input
-        value={username}
-        onChange={(e) => {
-          setUsername(e.target.value);
-        }}
-        className=" border placeholder:text-gray-400"
-        type="text"
-        placeholder="Name"
-      />
-      <input
-        value={email}
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-        className=" border placeholder:text-gray-400"
-        type="email"
-        placeholder="email"
-      />
-      <input
-        value={password}
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-        className=" border placeholder:text-gray-400"
-        type="password"
-        placeholder="password"
-      />
-      <button
-        type="submit"
-        onClick={() => {
-          registerUser(username, email, password);
-        }}
-      >
-        Create
-      </button>
+    <div className="flex w-screen h-[calc(100vh-4rem)] justify-center items-center">
+        <div className="flex flex-col m-8  rounded-lg w-96 overflow-hidden border-indigo-800 b border-2 justify-center">
+          <span className="text-center py-3 text-lg bg-indigo-800 text-white">SIGN UP</span>
+            {showMessage && <span className={`text-white bg-red-400 text-lg mx-8 mt-4 p-2`}>{ showMessage && `${message}`}</span>}
+            <form className="flex flex-col px-8 my-4">
+              <label for="name" className="block mb-2">Name</label>
+              <input id="name"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+                className=" mb-4 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                type="text"
+                placeholder="Name"
+              />
+              <label for="email" className="block mb-1">Email</label>
+              <input id="email"
+                className="mb-4 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" type="email" placeholder="example@mail.com"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <label for="password" className="block mb-1">Password</label>
+              <input id="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                className="mb-4 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                type="password"
+                placeholder="password"
+              />
+              <label for="cnfrmpassword" className="block mb-1">Confirm Password</label>
+              <input id="cnfrmpassword"
+                className="mb-4 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                type="password"
+                placeholder="password"
+              />
+              <button
+              className="bg-indigo-600 mb-6 text-white px-4 py-2 rounded-md hover:bg-indigo-700"
+                type="button"
+                onClick={() => {
+                  registerUser(username, email, password);
+
+                }}>
+                Create
+              </button>
+              <Link className="m-2 self-center" to="/login">Already have an account? <span className="text-indigo-500">Login</span></Link>
+            </form>
+        </div>
     </div>
   );
 };
